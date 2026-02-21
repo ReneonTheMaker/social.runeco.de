8ad3cb163ddf6dd46f52aaa13bc0c4d593a59cbf
ReneonTheMaker <andrewlpriv@gmail.com>
2026-02-21 18:17:56 +0000

    fix bug for prod

--- buildforprod.sh
+++ buildforprod.sh
@@ -1,6 +1,19 @@
 #!/bin/bash
+#!/bin/bash
+set -e
+
+echo "Building Tailwind..."
 npx tailwindcss -i ./tailwind.css -o ./static/css/tailwind.css --minify
+
+echo "Preparing dist..."
+rm -rf dist
 mkdir -p dist
+
 cp -r static dist/
-cp -r templates dist/
-go build -o dist/app main.go
+cp -r views dist/
+cp config.ini dist/
+
+echo "Building Go binary..."
+CGO_ENABLED=1 GOOS=linux GOARCH=amd64 go build -ldflags="-s -w" -o dist/app
+
+echo "Done."
